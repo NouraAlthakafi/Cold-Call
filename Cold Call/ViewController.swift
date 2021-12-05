124583cffca43e403467fcc3fc6ad039b21e9d2f
NouraAlthakafi <nouraal-thakafi@hotmail.com>
2021-12-05 21:27:28 +0000

    Initial Commit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var btnCall: UIButton!
+    let namesArray = ["Tareq", "Eman", "Noura", "Asmaa", "Mohammed", "Nada", "Abdulaziz"]
+    
+    @IBAction func btnCallAction(_ sender: UIButton) {
+        for _ in namesArray.indices {
+            let randomName = Int.random(in: 0..<7)
+            lbName.text = namesArray[randomName]
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
